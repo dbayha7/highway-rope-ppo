d3a449509ee56e04907bf036243a762f484db6a2
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2025-03-24 17:22:27 -0400

    Add initial project files including Python version, cluster setup script, main application, and project configuration

---
+++ cluster-setup.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+module load cuda/12.4
+module load cudnn/9.0.0-cuda12
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from highway-rope-ppo!")
+
+
+if __name__ == "__main__":
+    main()
